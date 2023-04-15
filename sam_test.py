438f5ed773196fa87d72313c30e9a23ed161c236
Ping-Cheng <knight16729438@gmail.com>
2023-04-14 20:26:30 -0400

    rename folder

--- sam_test.py
+++ sam_test.py
@@ -4,7 +4,7 @@ from saami.visualizer import visualize_volume_SAM
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE' #Walkaround
  
-spine_data = VolumeDataset(os.path.join(os.getcwd(), "data/MRI_dataset"))[0]
+spine_data = VolumeDataset(os.path.join(os.getcwd(), "data/MRI_sample"))[0]
 print(spine_data)
 spine_sam_data = get_volume_SAM(spine_data)
 print(spine_sam_data)
