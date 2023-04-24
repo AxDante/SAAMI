52b67baa1f07a68db5b536daf177ab3fbdcae55c
AxDante <knight16729438@gmail.com>
2023-04-23 23:36:19 -0400

    added some function for LCTSC

--- saami/preprocess_dicom_LCTSC.py
+++ saami/preprocess_dicom_LCTSC.py
@@ -3,6 +3,9 @@ import pydicom
 import numpy as np
 import nibabel as nib
 import dicom2nifti
+import SimpleITK as sitk
+from dicompylercore import dicomparser
+from dcmrtstruct2nii import dcmrtstruct2nii, list_rt_structs
 
 def create_output_dirs(input_path, output_path):
     for root, dirs, files in os.walk(input_path):
@@ -24,27 +27,33 @@ def convert_rtstruct_to_nifti(parent_folder, output_file):
 def convert_dicom_to_nifti(dicom_folder, output_nifti_file):
     dicom2nifti.convert_directory(dicom_folder, output_nifti_file)
 
+def get_adj_folder(path):
+    parent_folder = os.path.dirname(path)
+    subfolders = os.listdir(parent_folder)
+    subfolders.remove(os.path.basename(path))
+    return os.path.join(parent_folder, subfolders[0])
+
 def convert_dicom_to_nifti(input_path, output_path):
     for root, dirs, files in os.walk(input_path):
         dicom_files = [file for file in files if file.lower().endswith(".dcm")]
+
         if dicom_files:
             rtstruct_file = None
             for file in dicom_files:
                 if is_rtstruct(os.path.join(root, file)):
                     rtstruct_file = os.path.join(root, file)
                     break
-
-            output_dir = os.path.join(output_path, os.path.relpath(root, input_path))
-            os.makedirs(output_dir, exist_ok=True)
-
+        
             if rtstruct_file:
-                output_file = os.path.join(output_dir, 'rtstruct.nii.gz')
-                parent_folder = os.path.dirname(rtstruct_file)
-                print('output file is : ', output_file)
-                convert_rtstruct_to_nifti(parent_folder, output_file)
-            else:
-                pass
-                #convert_dicom_to_nifti(dicom_folder, output_nifti_file)
+
+                print('Processing data from : ', root)
+                output_dir = os.path.join(output_path, os.path.relpath(root, input_path))
+                adj_folder = get_adj_folder(root)
+                dcmrtstruct2nii(rtstruct_file,
+                                adj_folder, 
+                                output_dir)
+                image_file = os.path.join(output_dir, 'image.nii.gz')
+                os.rename(image_file, os.path.join(get_adj_folder(output_dir), 'image.nii.gz'))
 
 
 def main():
@@ -56,4 +65,5 @@ def main():
     convert_dicom_to_nifti(input_main_folder, output_main_folder)
 
 if __name__ == "__main__":
+
     main()
